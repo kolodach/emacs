9193d58895f9115419c159ea82cabd5965a84739
Oleg Bezhenar <obezhenar@sharpminds.com>
2019-03-06 16:15:27 +0200

    Foo

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,20 +1,16 @@
 					; theme
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (misterioso)))
  '(package-selected-packages (quote (yasnippet auto-complete yasnippet-snippets))))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 100 :family "Hack"))))
  )
 
 (menu-bar-mode -1)
